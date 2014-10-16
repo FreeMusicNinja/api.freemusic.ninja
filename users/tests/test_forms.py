a7c7a0ea10af35172b5f9b7faf79aa3dea00f82d
Micah Denbraver <macromicah@gmail.com>
2014-10-15 23:07:24 -0700

    Test user saving

--- users/tests/test_forms.py
+++ users/tests/test_forms.py
@@ -3,6 +3,7 @@ import pytest
 from django.core.exceptions import ValidationError
 
 from .. import forms
+from ..models import User
 
 
 class UserCreationFormTest(TestCase):
@@ -25,3 +26,26 @@ class UserCreationFormTest(TestCase):
         with pytest.raises(ValidationError) as excinfo:
             form.clean()
         assert form.PASSWORDS_DONT_MATCH in str(excinfo.value)
+
+    @pytest.mark.django_db
+    def test_committed_saving(self):
+        test_password = "test valid password"
+        form = forms.UserCreationForm(data={
+            'name': "Joe",
+            'email': "joe@example.com",
+            'password1': test_password,
+            'password2': test_password,
+        })
+        saved = form.save()
+        retrieved = User.objects.get(pk=saved.pk)
+        assert saved == retrieved
+
+    def test_uncommitted_saving(self):
+        test_password = "test valid password"
+        form = forms.UserCreationForm(data={
+            'name': "Joe",
+            'email': "joe@example.com",
+            'password1': test_password,
+            'password2': test_password,
+        })
+        assert form.save(commit=False).pk is None
