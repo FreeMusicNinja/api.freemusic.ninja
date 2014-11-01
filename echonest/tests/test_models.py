9a5a24b12279011624a49a6f9e3880d1a7825092
Micah Denbraver <macromicah@gmail.com>
2014-10-31 22:45:58 -0700

    Let echonest tests get found by pytest
    pytest looks for tests in 'test_*.py' files.

--- echonest/tests/test_models.py
+++ echonest/tests/test_models.py
@@ -2,7 +2,7 @@ import json
 from unittest import TestCase
 from unittest.mock import patch
 
-from .models import SimilarResponse
+from ..models import SimilarResponse
 
 
 class SimilarResponseModelTest(TestCase):
