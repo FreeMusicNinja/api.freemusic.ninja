3e766448bfff4c96e8ca893a25d7d16a557b6557
Micah Denbraver <macromicah@gmail.com>
2014-12-20 20:50:28 -0800

    Add complete set of fields to fma.Artist

--- fma/models.py
+++ fma/models.py
@@ -1,12 +1,34 @@
 from django.db import models
+import jsonfield
 from model_utils.models import TimeStampedModel
 
 
 class Artist(TimeStampedModel):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=250)
+    handle = models.CharField(max_length=250, null=True)
     url = models.URLField(max_length=2000)
+    name = models.CharField(max_length=250)
+    bio = models.CharField(max_length=50000, null=True)
+    members = models.CharField(max_length=15000, null=True)
     website = models.URLField(null=True)
+    wikipedia_page = models.URLField(null=True)
+    donation_url = models.URLField(null=True)
+    contact = models.CharField(max_length=200, null=True)
+    active_year_begin = models.CharField(max_length=4, null=True)
+    active_year_end = models.CharField(max_length=4, null=True)
+    related_projects = models.CharField(max_length=2000, null=True)
+    associated_labels = models.CharField(max_length=500, null=True)
+    comments = models.IntegerField(null=True)
+    favorites = models.IntegerField(null=True)
+    date_created = models.DateTimeField(null=True)
+    flattr_name = models.CharField(max_length=100, null=True)
+    paypal_name = models.CharField(max_length=100, null=True)
+    latitude = models.CharField(max_length=20, null=True)
+    longitude = models.CharField(max_length=20, null=True)
+    image_file = models.URLField(null=True)
+    location = models.CharField(max_length=500, null=True)
+    tags = jsonfield.JSONField(null=True)
+    images = jsonfield.JSONField(null=True)
 
     class Meta:
         ordering = ('name',)
