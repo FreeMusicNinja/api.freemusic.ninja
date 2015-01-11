6d74eed1b9b34a4803a77d05e9c19d940712f18b
Trey Hunner <trey@treyhunner.com>
2015-01-11 12:41:04 -0800

    Improve appearance of similarities in admin

--- similarities/admin.py
+++ similarities/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import GeneralArtist, Similarity
 
+
+class SimilarityModelAdmin(admin.ModelAdmin):
+    fields = ['other_artist', 'cc_artist', 'weight']
+    list_display = ['id', 'other_artist', 'cc_artist', 'weight']
+    readonly_fields = ['other_artist', 'cc_artist']
+
+
 admin.site.register(GeneralArtist)
-admin.site.register(Similarity)
+admin.site.register(Similarity, SimilarityModelAdmin)
