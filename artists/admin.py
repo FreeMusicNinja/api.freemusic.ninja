1cf0b50bef895fe971f0f4921ade7149e02d04b3
Trey Hunner <trey@treyhunner.com>
2015-01-11 12:35:34 -0800

    Add search fields to artist model admin lists

--- artists/admin.py
+++ artists/admin.py
@@ -5,7 +5,23 @@ from fma.admin import query_from_api
 from .models import Artist, JamendoArtist, MagnatuneArtist, FMAArtist
 
 
-admin.site.register(Artist)
-admin.site.register(JamendoArtist)
-admin.site.register(MagnatuneArtist)
-admin.site.register(FMAArtist, actions=(query_from_api,))
+class ArtistModelAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class JamendoArtistModelAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+class MagnatuneArtistModelAdmin(admin.ModelAdmin):
+    search_fields = ['artist']
+
+
+class FMAArtistModelAdmin(admin.ModelAdmin):
+    search_fields = ['artist_name']
+
+
+admin.site.register(Artist, ArtistModelAdmin)
+admin.site.register(JamendoArtist, JamendoArtistModelAdmin)
+admin.site.register(MagnatuneArtist, MagnatuneArtistModelAdmin)
+admin.site.register(FMAArtist, FMAArtistModelAdmin, actions=(query_from_api,))
